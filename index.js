9aa4598cf3cf0391dbff4632066994fd088d2d99
dmonasterios <monasterio13septiembre@gmail.com>
2025-06-12 06:49:35 -0500

    Agregada nueva URL de origen para CORS en la configuración de index.js, permitiendo solicitudes desde el backend de EmotionQuest. Esto mejora la integración y seguridad de las solicitudes entre el frontend y el backend.

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://emotionquest-backend-189934902436.us-central1.run.app",
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true,
